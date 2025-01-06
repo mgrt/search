0cc832a9f52fc9258d795681763895a1f146a411
mgrt <80220360+mgrt@users.noreply.github.com>
2025-01-06 03:53:40 +0100

    search

---
+++ search/scripts/test_time_compute.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# Copyright 2024 The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+
+import torch
+from vllm import LLM
+
+from sal.config import Config
+from sal.models.reward_models import load_prm
+from sal.search import beam_search, best_of_n, dvts
+from sal.utils.data import get_dataset, save_dataset
+from sal.utils.parser import H4ArgumentParser
+from sal.utils.score import score
+import gc
+
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+APPROACHES = {
+    "beam_search": beam_search,
+    "dvts": dvts,
+    "best_of_n": best_of_n,
+}
+
+
+def main():
+    parser = H4ArgumentParser(Config)
+    config = parser.parse()
+
+    approach_fn = APPROACHES[config.approach]
+
+    num_gpus = torch.cuda.device_count()
+    print("load llm")
+    llm = LLM(
+        model=config.model_path,
+        gpu_memory_utilization=0.7,
+        enable_prefix_caching=True,
+        seed=config.seed,
+        tensor_parallel_size=num_gpus,
+        dtype='half',
+    )
+    print("Loaddd prmm*****************************")
+    prm = load_prm(config)
+    print("Baed load prm*****************************")
+    dataset = get_dataset(config)
+    dataset = dataset.map(
+        approach_fn,
+        batched=True,
+        batch_size=config.search_batch_size,
+        fn_kwargs={"config": config, "llm": llm, "prm": prm},
+        desc="Running search",
+        load_from_cache_file=False,
+    )
+    print("start scoreee***********************")
+    gc.collect()
+    dataset = score(dataset, config)
+    print("fin save***********************")
+    save_dataset(dataset, config)
+    logger.info("Done 🔥!")
+
+
+if __name__ == "__main__":
+    main()
